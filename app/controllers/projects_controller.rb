3d3ee692ca37b9217d9831d38fc8f8ba7d08b826
Nick Johnson <njohnson7@users.noreply.github.com>
2018-08-06 16:07:05 -0700

    added project show and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,24 @@
 class ProjectsController < ApplicationController
   def index
-    # render 'asd'
-    # render :index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find params[:id]
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new params.require(:project).permit(:name, :description)
+    if @project.save
+      flash[:success] = 'Project was successfully created.'
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,4 +29,9 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def params__
+
+    end
 end
